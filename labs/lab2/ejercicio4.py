5e884d1a2ee6391d68f0b266fca5e2c0654da1a8
Gustavo <cru22779@uvg.edu.gt>
2025-02-11 17:32:22 -0600

    Added: ejercicio4

---
+++ labs/lab2/ejercicio4.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def generar_muestra_gaussiana(n, media, covarianza):
+    """
+    Genera una muestra aleatoria de una distribución gaussiana multivariada
+    de dimensión n con la media y covarianza especificadas.
+    """
+    muestra = np.random.multivariate_normal(media, covarianza, size=1000)
+    df_muestra = pd.DataFrame(muestra, columns=[f'Variable_{i+1}' for i in range(n)])
+    return df_muestra
+
+def verificar_media_covarianza(muestra, media_teorica, covarianza_teorica):
+    """
+    Compara la media y la covarianza de la muestra con los valores teóricos.
+    """
+    media_muestra = muestra.mean()
+    covarianza_muestra = muestra.cov()
+
+    print(f'Media teórica: {media_teorica}')
+    print(f'Media de la muestra: {media_muestra}')
+    print(f'Covarianza teórica:\n{covarianza_teorica}')
+    print(f'Covarianza de la muestra:\n{covarianza_muestra}')
+    
+    return media_muestra, covarianza_muestra
+
+def graficar_pairplot(muestra):
+    """
+    Graficar un pairplot para visualizar las densidades y nubes de puntos
+    entre las variables de la muestra.
+    """
+    sns.pairplot(muestra)
+    plt.title('Pairplot de la Muestra Aleatoria Gaussiana')
+    plt.show()
+
+n = 4 
+media_teorica = np.array([0, 0, 0, 0]) 
+covarianza_teorica = np.array([[1, 0.5, 0.3, 0.1],
+                                [0.5, 1, 0.6, 0.2],
+                                [0.3, 0.6, 1, 0.4],
+                                [0.1, 0.2, 0.4, 1]]) 
+
+muestra = generar_muestra_gaussiana(n, media_teorica, covarianza_teorica)
+
+verificar_media_covarianza(muestra, media_teorica, covarianza_teorica)
+
+graficar_pairplot(muestra)
+
